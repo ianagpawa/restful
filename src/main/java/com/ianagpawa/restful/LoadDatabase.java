750301d6494f5fcb7e521311b411d9521506ff13
Ian Agpawa <agpawaji@gmail.com>
2020-11-08 23:42:58 -0500

    Consolidate loadDatabase

--- src/main/java/com/ianagpawa/restful/LoadDatabase.java
+++ src/main/java/com/ianagpawa/restful/LoadDatabase.java
@@ -1,6 +1,10 @@
-package com.ianagpawa.restful.quote;
+package com.ianagpawa.restful;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ianagpawa.restful.project.Project;
+import com.ianagpawa.restful.project.ProjectRepository;
+import com.ianagpawa.restful.quote.Quote;
+import com.ianagpawa.restful.quote.QuoteRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -33,6 +37,26 @@ public class LoadDatabase {
                 log.info("Quotes loaded!");
             } catch (IOException e){
                 log.info("Unable to load quotes: " + e.getMessage());
+            }
+        };
+    }
+
+    @Bean
+    CommandLineRunner initProjectDatabase(ProjectRepository repository) {
+        return args -> {
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference<List<Project>> typeReference = new TypeReference<List<Project>>(){};
+            InputStream inputStream = TypeReference.class.getResourceAsStream("/json/projects.json");
+            try {
+                List<Project> projects = mapper.readValue(inputStream,typeReference);
+                projects
+                        .stream()
+                        .forEach((project) -> {
+                            log.info("Preloading " + repository.save(project));
+                        });
+                log.info("Projects loaded!");
+            } catch (IOException e){
+                log.info("Unable to load projects: " + e.getMessage());
             }
         };
     }
